6da7758537bed9f35b3869ceab5e4ca8ae31779c
ivuelekwa stephen <streetcoder99@gmail.com>
2022-03-12 11:52:16 +0100

    Initial commit

---
+++ crypto_empire/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+      <h1 style={{color: 'midnightblue'}}>Crypto Empire</h1>
+    </div>
+  );
+}
+
+export default App;
