05828dcc29397bf9056138570312c350ba53a2eb
ivuelekwa stephen <streetcoder99@gmail.com>
2022-03-16 13:45:25 +0100

    Add routes with corresponding components

--- crypto_empire/src/App.js
+++ crypto_empire/src/App.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Routes, Link, Route } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
+import { Navbar, Homepage, Exchanges, CryptoDetails, Cryptocurrencies } from './components';
+import './App.css'
 
 function App() {
   return (
-    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
-      <h1 style={{color: 'midnightblue'}}>Crypto Empire</h1>
+    <div className='app'>
+      <div className='navbar'>
+        <Navbar />
+      </div>
+      <div className='main'>
+        <Layout>
+          <div> 
+            <Routes>
+              <Route exact path='/' element={<Homepage />} />
+              <Route exact path='/cryptocurrencies' element={<Cryptocurrencies />} />
+              <Route exact path='/exchanges' element={<Exchanges />} />
+              <Route exact path='/crypto/:coinid' element={<CryptoDetails />} />
+            </Routes>
+          </div>
+        </Layout>
+
+      </div>
+      <div className='footer'>
+
+      </div>
     </div>
   );
 }
