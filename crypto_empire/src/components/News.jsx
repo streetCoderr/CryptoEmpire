a5bd38fb4c53adadd1bc638ed2e2f69f2f1bc0f7
ivuelekwa stephen <streetcoder99@gmail.com>
2022-03-18 11:45:41 +0100

    Narrow down the scope of the News component to be able to provide news about a specific coin selected by the user

--- crypto_empire/src/components/News.jsx
+++ crypto_empire/src/components/News.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi'
+import { useGetCryptosQuery } from '../services/cryptoApi';
 import { Card, Row, Col, Select, Typography, Avatar } from 'antd'
 import moment from 'moment';
 
@@ -11,16 +12,43 @@ const News = ({ simplified }) => {
   const count = simplified ? 6 : 12;
   const [newsCategory, setNewsCategory] = useState('cryptocurrencies')
   const { data: cryptoNews, isFetching } = useGetCryptoNewsQuery({newsCategory, count})
+  const { data: cryptoCoins } = useGetCryptosQuery(100)
   console.log(cryptoNews)
   if (isFetching) return 'Loading...'
   return (
     <Row gutter={[24, 24]} className='divs'>
+      {
+        !simplified && 
+        (
+          <Col span={24}>
+          <center>
+            <Select
+              style={{ margin: '0 auto', minWidth: '200px'}}
+              showSearch
+              value={newsCategory}
+              placeholder="Select a crypto coin"
+              optionFilterProp="children"
+              onChange={(value) => setNewsCategory(value)}
+              filterOption={(input, option) =>
+                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+              }
+            >
+              <Option value="cryptocurrencies">Cryptocurrencies</Option>
+              {cryptoCoins?.data?.coins?.map((coin) => (
+                <Option value={coin?.name} key={coin?.uuid}>{coin?.name}</Option>
+              ))}
+            </Select>
+          </center>
+          </Col>
+        )
+      }
+
       {cryptoNews.value.map((news) => (
         <Col xs={24} sm={12} lg={8} key={news.url}>
           <Card hoverable className='news-card'>
             <a href={news?.url} rel='noreferrer' target='_blank'>
               <div className='news-image-container'>
-                <Title className='news-title' level={5}>{news?.name}</Title>
+                <Title className='news-title' level={5}>{news?.name.length > 85 ? `${news?.name.substring(0, 85)}...` : `${news?.name}`}</Title>
                 <img style={{maxHeight: '100px', maxWidth: '200px'}} alt={news?.name} src={news?.image?.thumbnail?.contentUrl || demoImage} />
               </div>
                 <p>{news?.description?.length > 100 ? `${news?.description?.substring(0, 100)}...` : news?.description}</p>
